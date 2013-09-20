2302f0ee1d93273057d6e0e4527f39f50a9b83aa
Thomas Spycher <tspycher@zerodine.com>
2013-09-20 17:58:24 +0200

    addes Sphinx Documentation Support

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Krypton',
+    version='0.1',
+    packages=['src', 'src.hkpserver', 'src.hkpserver.libs', 'src.hkpserver.libs.gossip', 'src.hkpserver.libs.gpgmongo',
+              'src.hkpserver.libs.gpgjsonparser', 'src.hkpserver.controllers'],
+    url='https://github.com/zerodine/krypton',
+    license='MIT',
+    author='tspycher',
+    author_email='me@tspycher.com',
+    description='GPG/PGP Keyserver with many enhancements'
+)
---
+++ src/index.rst
@@ -0,0 +1,22 @@
+.. Krypton Keyserver documentation master file, created by
+   sphinx-quickstart on Fri Sep 20 17:37:16 2013.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Krypton Keyserver's documentation!
+=============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
