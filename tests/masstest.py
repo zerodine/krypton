8efb55a8c7828a77bc27251908311fb4849f9006
Thomas Spycher <tspycher@zerodine.com>
2013-09-24 07:14:55 +0200

    Added jmeter testcase for performance tests
    Signed-off-by: Thomas Spycher <tspycher@zerodine.com>

--- tests/masstest.py
+++ tests/masstest.py
@@ -1,19 +1,36 @@
 import gnupg
 import os
+from src.hkpserver.libs.gpgmongo import GpgModel
+from tests.abstracttestcase import AbstractTestCase
 
 __author__ = 'thospy'
 
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
+class MassTest(AbstractTestCase):
     gpg = None
     basefolder = "demodata/mass"
     numKeys = 1000
+    gpgModel = None
 
     def setUp(self):
+        self.gpgModel = GpgModel(connectionUrl=self.config.mongoConnectionUrl)
+        self.gpgModel.connect(db=self.config.mongoDatabase)
+        self.gpgModel.collection = self.config.mongoCollection
+
         self.gpg = gnupg.GPG(gnupghome='/tmp', gpgbinary="/usr/local/bin/gpg")
 
+    @unittest.skip("Do need to import the keys again")
+    def test_loadTestdata(self):
+        for d in os.listdir(self.basefolder):
+            for f in os.listdir("%s/%s" % (self.basefolder, d)):
+                keyFile = "%s/%s/%s" % (self.basefolder, d, f)
+                key = self._readKey(keyFile)
+                self.gpgModel.uploadKey(key)
+                print "Imported key %s" % keyFile
+
+    @unittest.skip("Do need to create more keys")
     def test_createbulkkeys(self):
         for i in range(self.numKeys):
             input_data = self.gpg.gen_key_input(key_type="RSA", key_length=1024*4)
