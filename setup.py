62f64adbf074c5bc127d69c022f44d2fc82a1bb0
Thomas Spycher <tspycher@zerodine.com>
2013-09-26 10:40:41 +0200

    put package_data back again into setup.py
    Signed-off-by: Thomas Spycher <tspycher@zerodine.com>

--- setup.py
+++ setup.py
@@ -12,15 +12,15 @@ setup(
     author='tspycher',
     author_email='me@tspycher.com',
     description='GPG/PGP Keyserver with many enhancements',
-    install_requires=["PIL", "qrcode", "six", "tornado", "pymongo", "pgpdump", "M2Crypto"]
-    #package_data={'krypton.hkpserver': [
-    #    'views/themes/*.html',
-    #    'views/*.html',
-    #    'wwwroot/css/*.css',
-    #    'wwwroot/fonts/*',
-    #    'wwwroot/img/*',
-    #    'wwwroot/js/vendor/*.js',
-    #    'wwwroot/js/*.js',
-    #    'wwwroot/*']}
+    install_requires=["PIL", "qrcode", "six", "tornado", "pymongo", "pgpdump", "M2Crypto"],
+    package_data={'krypton.hkpserver': [
+        'views/themes/*.html',
+        'views/*.html',
+        'wwwroot/css/*.css',
+        'wwwroot/fonts/*',
+        'wwwroot/img/*',
+        'wwwroot/js/vendor/*.js',
+        'wwwroot/js/*.js',
+        'wwwroot/*']}
     #data_files = [('krypton.hkpserver', ['negar/data/untouchable.dat'])],
 )
